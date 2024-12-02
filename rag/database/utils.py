ab004641ba2913470eecefce2910dcacee6d34f9
nj-guiqi <2020150501012@std.uestc.edu.cn>
2024-12-02 12:29:00 +0800

    实现密集搜索和稀疏搜索功能，支持通过 MilvusService 进行查询

--- rag/database/utils.py
+++ rag/database/utils.py
@@ -1,6 +1,23 @@
-def dense_search(query_embeddings, topk=10):
-    pass
+from typing import List, Dict, Any
+from .milvus import MilvusClientWrapper
+from .milvus import MilvusService
 
 
-def sparse_search(query_embeddings, topk=10):
+def dense_search(service:MilvusService, collection_name, query_embedding:list, output_fields, topk=10):
+    return service.search(collection_name = collection_name,
+            query_embedding = query_embedding,
+            output_fields = output_fields,
+            search_by="dense",
+            topk = topk)
+    
+
+def sparse_search(service:MilvusService, collection_name, query_embedding:list, output_fields, topk=1):
+        return service.search(collection_name = collection_name,
+            query_embedding = query_embedding,
+            output_fields = output_fields,
+            search_by="sparse",
+            topk = topk)
+
+
+def colbert_search(query_embeddings, topk=10):
     pass
