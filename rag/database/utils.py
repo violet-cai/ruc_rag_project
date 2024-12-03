292fa4858f7b06ef5b49380b747de611fe1e8e6d
nj-guiqi <2020150501012@std.uestc.edu.cn>
2024-12-03 18:05:46 +0800

    重构密集搜索和稀疏搜索功能，移除服务参数，改为从配置文件读取集合名称和输出字段

--- rag/database/utils.py
+++ rag/database/utils.py
@@ -1,23 +1,25 @@
 from typing import List, Dict, Any
 from .milvus import MilvusClientWrapper
 from .milvus import MilvusService
+from ..config.config import Config
 
-
-def dense_search(service:MilvusService, collection_name, query_embedding:list, output_fields, topk=10):
-    return service.search(collection_name = collection_name,
+service = MilvusService(MilvusClientWrapper(host="localhost", port="19530"),"")
+config = Config()
+    
+def dense_search(query_embedding:list, topk=10):
+        return service.search(collection_name = config["db_collection_name"],
             query_embedding = query_embedding,
-            output_fields = output_fields,
+            output_fields = config["db_output_fields"],
             search_by="dense",
             topk = topk)
     
 
-def sparse_search(service:MilvusService, collection_name, query_embedding:list, output_fields, topk=1):
-        return service.search(collection_name = collection_name,
+def sparse_search(query_embedding:list, topk=10):
+        return service.search(collection_name = config["db_collection_name"],
             query_embedding = query_embedding,
-            output_fields = output_fields,
+            output_fields = config["db_output_fields"],
             search_by="sparse",
             topk = topk)
-
 
 def colbert_search(query_embeddings, topk=10):
     pass
