c75b31b2878add9a774795aa26ec97d118343fd5
lishiyu <3157906397@qq.com>
2024-12-26 23:48:42 +0800

    feat&fix: add bing_retrieval, corrected the formatting of engine results

--- rag/dataprocess/dataloader.py
+++ rag/dataprocess/dataloader.py
@@ -2,20 +2,25 @@ import json
 from ..config.config import Config
 import os
 import requests
-import jieba
+import jieba  
 import re
+
 from bs4 import BeautifulSoup
 from readability import Document
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
 
 def load_data_from_json(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def load_stopwords_form_txt(file_path):
-    with open(file_path, encoding="utf-8") as f:
+    with open(file_path, encoding='utf-8') as f:
         stopwords = {line.strip() for line in f.readlines()}
     return stopwords
 
@@ -23,138 +28,263 @@ def load_stopwords_form_txt(file_path):
 def load_dataset(dataset_name):
     """
     根据数据集名称加载相应的数据集.
-
+    
     参数：
     dataset_name (str): 数据集名称。
-
+    
     取值：
     dataset: regulations数据集;
     qa: qa数据集。
-
+    
     返回：
     dict: 加载的数据集
     """
     config = Config()
-    if dataset_name == "dataset":
-        dataset_path = config["corpus_path"]
-    elif dataset_name == "qa":
-        dataset_path = config["qa_path"]
-
+    if dataset_name == 'corpus':
+        dataset_path = config['corpus_path']
+    elif dataset_name == 'qa':
+        dataset_path = config['qa_path']
+        
     if not dataset_path:
         raise ValueError(f"未找到名为 {dataset_name} 的数据集路径。")
     current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, "..", "..", dataset_path)
-    file_path = os.path.normpath(file_path)
+    if 'data/' in dataset_path:
+       dataset_path = dataset_path.replace('data/', '') 
+    file_path = os.path.join(current_dir, dataset_path)
     data_set = load_data_from_json(file_path)
-
+    
     return data_set
 
+def get_datasets():
+    dataset_names = ["corpus", "qa"]
+    ret = []
+    for dataset_name in dataset_names:
+        dataset = load_dataset(dataset_name)
+        ret.append(dataset)
+    return ret
 
 def load_stopwords():
     config = Config()
-
-    stopword_path = config["stopword_path"]
+    
+    stopword_path = config['stopword_path']
     current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, "..", "..", stopword_path)
-    file_path = os.path.normpath(file_path)
+    if 'data/' in stopword_path:
+       stopword_path = stopword_path.replace('data/', '') 
+    file_path = os.path.join(current_dir, stopword_path)
     stopwords = load_stopwords_form_txt(file_path)
     return stopwords
 
-
 def get_dict_path():
     config = Config()
-
-    dict_path = config["dict_path"]
+    
+    dict_path = config['dict_path']
     current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, "..", "..", dict_path)
-    return file_path
+    if 'data/' in dict_path:
+       dict_path = dict_path.replace('data/', '') 
+    dict_path = os.path.join(current_dir, dict_path)
+    
+    return dict_path
 
 
 def get_keywords(query):
     dict_path = get_dict_path()
     jieba.load_userdict(dict_path)
-    new_data = "".join(re.findall("[\u4e00-\u9fa5]+", query))
+    new_data = ''.join(re.findall('[\u4e00-\u9fa5]+', query))
     stopwords = load_stopwords()
-
+    
     seg_list = jieba.lcut(new_data)
     # 去除停用词并且去除单字
     filtered_words = [word for word in seg_list if word not in stopwords and len(word) > 1]
     # 使用TF-IDF对关键词进行打分
     tfidf_vectorizer = TfidfVectorizer(max_features=10)
-    tfidf_matrix = tfidf_vectorizer.fit_transform([" ".join(filtered_words)])
+    tfidf_matrix = tfidf_vectorizer.fit_transform([' '.join(filtered_words)])
     feature_names = tfidf_vectorizer.get_feature_names_out()
     tfidf_scores = tfidf_matrix.toarray()[0]
 
     keywords_with_weights = [(feature_names[i], tfidf_scores[i]) for i in range(len(feature_names))]
-    keywords_with_weights.sort(key=lambda x: x[1], reverse=True)
-
+    keywords_with_weights.sort(key=lambda x: x[1], reverse=True)  
+    
     return keywords_with_weights
 
 
-def get_html(query=None, url=None, mode="search"):
+
+def get_html(query=None, url=None, mode='search'):
     """
     获取网页内容。
-
+    
     参数：
-    query: 搜索关键字(可选)
-    url: 搜索网址(可选)
-    mode: 工作模式(search: 搜索; extract: 提取)
-
-    返回：
-    html_content: 网页内容
+        query: 搜索关键字(可选)
+        url: 搜索网址(可选)
+        mode: 工作模式(search: 搜索; extract: 提取)
+    
+    返回值：
+        html_content: 网页内容
     """
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
     }
-    if mode == "search" and query is not None:
+    if mode == 'search' and query is not None:
         url = f"https://www.baidu.com/s?wd={query}"
-    elif mode == "extract" and url is not None:
+    elif mode == 'extract' and url is not None:
         url = url
 
     if url is not None:
-        response = requests.get(url, headers=headers)
-        response.encoding = "utf-8"  # 设置编码格式
-        if response.status_code == 200:
-            html_content = response.text
-            return html_content
-        else:
-            print("Error: Failed to retrieve HTML content!")
+        try:
+            response = requests.get(url, headers=headers)
+            response.encoding = 'utf-8'  # 设置编码格式
+            
+            if response.status_code == 200:
+                html_content = response.text
+                return html_content
+            else:
+                # print(f"Error: {response.status_code}")
+                return None
+        except requests.exceptions.RequestException as e:
+            # print(f"Error: {e}")
             return None
     else:
         print("Error: No URL provided!")
         return None
 
 
-def parse_html(html_content, mode="search"):
+def parse_html(html_content, mode='search'):
     """
     解析网页内容。
-
+    
     参数：
-    html_content: 网页内容
-    mode: 工作模式(search: 搜索; extract: 提取)
-
-    返回：
-    results: 解析结果
+        html_content: 网页内容
+        mode: 工作模式(search: 搜索; extract: 提取)
+    
+    返回值：
+        results: 解析结果
     """
     if html_content:
-        soup = BeautifulSoup(html_content, "html.parser")
+        soup = BeautifulSoup(html_content, 'lxml')
     else:
         # print("Error: HTML content is empty!")
         return None
-    if mode == "search":
+    if mode == 'search':
         results = []
-        for item in soup.find_all("div", class_="result"):
-            title = item.find("h3").get_text()
-            link = item.find("a")["href"]
-            results.append({"title": title, "link": link})
-    elif mode == "extract":
+        for item in soup.find_all('div', class_='result'):
+            title = item.find('h3').get_text()
+            link = item.find('a')['href']
+            results.append({'title': title, 'link': link})
+    elif mode == 'extract':
         doc = Document(html_content)
         content = doc.summary()
-        soup_content = BeautifulSoup(content, "html.parser")
+        soup_content = BeautifulSoup(content, 'lxml')
         content = soup_content.get_text(strip=True)
         # 未提取到内容则返回None
-        if any("\u4e00" <= char <= "\u9fff" for char in content):
+        if any('\u4e00' <= char <= '\u9fff' for char in content):
             results = content
         else:
             results = None
     return results
+
+
+def content_summary(content,language='chinese'):
+    """
+    生成内容摘要。
+    
+    参数:
+        content: 内容
+        language: 语言类型
+
+    返回值:
+        summary: 内容摘要
+    """
+    if not content or len(content.strip()) == 0:
+        print("输入内容为空")
+    sentence_count = 5
+    parser = PlaintextParser.from_string(content, Tokenizer(language))
+    stemmer = Stemmer(language)
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(language)
+    
+    summary = ""
+    try:
+        for sentence in summarizer(parser.document, sentence_count):
+            summary += str(sentence) + " "
+    except Exception as e:
+        return f"摘要生成失败: {str(e)}"
+    return summary
+
+def baidu_search(query:str) -> list:
+    search_content = get_html(query=query,mode='search')
+    search_results = parse_html(search_content,'search')
+    if search_results:
+        return search_results
+    return None
+    
+
+def bing_search(query: str) -> list:
+    """
+    使用 Bing Search API 搜索查询并获取结果。
+    
+    参数：
+        query: 要搜索的查询字符串
+
+    返回值：
+        results: 搜索结果列表
+    """
+    url = "https://api.bing.microsoft.com/v7.0/search"
+    config = Config()
+    api_key = config['bing_api']
+    headers = {
+        "Ocp-Apim-Subscription-Key": api_key,
+        "Content-Type": "application/json"
+    }
+    params = {
+        "q": query,
+        "textDecorations": True,
+        "textFormat": "HTML",
+        "mtk": "zh-CN"  # 仅在中国市场搜索
+    }
+    response = requests.get(url, headers=headers, params=params)
+   
+    if response.status_code == 200:
+        search_results = response.json()
+        results = []
+        
+        if "webPages" in search_results:
+            for item in search_results["webPages"]["value"]:
+                results.append({
+                    "title": item["name"],
+                    "link": item["url"]
+                })
+        return results
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+    
+def get_docs(search_results:list):
+    """
+    获取检索结果中的文档内容。
+    
+    参数:
+        search_results (list): 检索结果列表
+
+    返回值:
+        retrieved_docs: 文档内容列表
+    """
+    retrieved_docs = []
+    max_len = 512
+    if search_results:
+        for result in search_results:
+            url = result['link']
+            title = result['title']
+            doc_content = get_html(url=url,mode='extract')
+            doc = parse_html(doc_content,'extract')
+            if doc:
+                doc = title + str(":") + str(doc)
+                doc = content_summary(doc,'chinese')
+                if len(doc) > max_len:
+                    doc = doc[:max_len]
+                elif len(doc) == 0:
+                    continue
+                retrieved_docs.append(doc)
+            else:
+                continue
+            
+    return retrieved_docs
+        
--- rag/retriever/retriever.py
+++ rag/retriever/retriever.py
@@ -1,22 +1,25 @@
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
 from typing import List, Dict, Tuple, Literal, Union
 
 import numpy as np
+from FlagEmbedding import BGEM3FlagModel
 
 from rag.config.config import Config
 from rag.database.utils import dense_search, sparse_search
-from rag.dataprocess.dataloader import load_dataset, get_keywords, get_html, parse_html
+from rag.dataprocess.dataloader import get_datasets, get_keywords, bing_search, baidu_search, get_docs
 from pymilvus import model
+
 
 
 class Retriever:
     def __init__(self, config: Config):
-        self.rrf_k = config["rrf_k"]  # RRF算法加权参数
-        self.topk = config["retriever_topk"]  # topk个文档
-        self.fields_to_search = config["fields_to_search"]
+        self.rrf_k = config["rrf_k"]    # RRF算法加权参数
+        self.topk = config["retriever_topk"]    # topk个文档
+        self.fields_to_search = config['fields_to_search']
         self.model = model.hybrid.BGEM3EmbeddingFunction(
             device="cuda:0", return_sparse=True, return_dense=True, return_colbert_vecs=False
-        )  # 采用BGE3模型
+        )   # 采用BGE3模型
 
     def _get_retrieved_lists(
         self,
@@ -25,8 +28,8 @@ class Retriever:
         ],
     ) -> Tuple[List[dict], List[dict]]:
         # 分别检索三种检索方法
-        dense_retrieved_list = dense_search(query_embedding=query_embedding["dense"][0], topk=self.topk * 3)  # 密集向量
-        sparse_retrieved_list = sparse_search(query_embedding=query_embedding["sparse"], topk=self.topk)  # 稀疏向量
+        dense_retrieved_list = dense_search(query_embedding=query_embedding["dense"][0], topk=self.topk * 3)    # 密集向量
+        sparse_retrieved_list = sparse_search(query_embedding=query_embedding["sparse"], topk=self.topk)    # 稀疏向量
         # 不确定检索时间消耗，并行反而可能减速
         # with ThreadPoolExecutor() as executor:
         #     dense_future = executor.submit(dense_search, query_embedding["dense_vecs"][0], topk=self.topk * 5)
@@ -42,73 +45,68 @@ class Retriever:
             scores[doc] += 1 / (self.rrf_k + idx + 1)
         return [doc for doc, _ in sorted(scores.items(), key=lambda item: item[1], reverse=True)][: self.topk]
 
-    def _deduplicate_merge(self, dense_retrieved_list, sparse_retrieved_list):
-        dense_set = set(dense_retrieved_list)
-        sparse_set = set(sparse_retrieved_list)
-        merged_set = dense_set.union(sparse_set)
-        merged_list = list(merged_set)
-        return merged_list
-
     def retrieve(self, query_list: List[str]) -> List[List[str]]:
         retrieved_docs = []
         for query in query_list:
             query_embedding = self.model([query])
             dense_retrieved_list, sparse_retrieved_list = self._get_retrieved_lists(query_embedding)
-            docs = self._deduplicate_merge(dense_retrieved_list, sparse_retrieved_list)
+            docs = self._rrf(dense_retrieved_list, sparse_retrieved_list)
             retrieved_docs.append(docs)
         return retrieved_docs
 
+
     def retrieve_with_keywords(self, query_list: List[str]) -> List[List[str]]:
-        retrieved_docs1 = []
-        retrieved_docs2 = []
-        dataset1 = load_dataset("dataset")
-        dataset2 = load_dataset("qa")
-        fields_to_search = self.fields_to_search
+        retrieved_docs = []
+        datasets = get_datasets()
+        
         for query in query_list:
             keywords_with_weights = get_keywords(query)
             # 权重前5的关键词用于检索
             top_weighted_keywords = [kw for kw, weight in keywords_with_weights[:5]]
-
-            keyword_docs1 = []
-            for document in dataset1:
-                matched_keywords_count = 0
-                for keyword in top_weighted_keywords:
-                    if any(keyword in str(document.get(field, "")) for field in fields_to_search):
-                        matched_keywords_count += 1
-                # 如果关键词匹配度超过50%,则将文档添加到检索结果
-                if matched_keywords_count / len(top_weighted_keywords) >= 0.5:
-                    doc_content = document.get("content", "")
-                    keyword_docs1.append((doc_content, matched_keywords_count))
-            keyword_docs1.sort(key=lambda x: x[1], reverse=True)
-            retrieved_docs1.append([doc[0] for doc in keyword_docs1])
-
-            keyword_docs2 = []
-            for document in dataset2:
-                matched_keywords_count = 0
-                for keyword in top_weighted_keywords:
-                    if any(keyword in str(document.get(field, "")) for field in fields_to_search):
-                        matched_keywords_count += 1
-                # 如果关键词匹配度超过50%,则将文档添加到检索结果
-                if matched_keywords_count / len(top_weighted_keywords) >= 0.5:
-                    doc_content = document.get("content", "")
-                    keyword_docs2.append((doc_content, matched_keywords_count))
-            keyword_docs2.sort(key=lambda x: x[1], reverse=True)
-            retrieved_docs2.append([doc[0] for doc in keyword_docs2])
-
-        return retrieved_docs1[: self.topk] + retrieved_docs2[: self.topk]
-
-    def retrieve_with_engine(self, query: str) -> List[str]:
-        search_content = get_html(query=query, mode="search")
-        search_results = parse_html(search_content, "search")
-
-        retrieved_docs = []
-        for result in search_results:
-            url = result["link"]
-            title = result["title"]
-            doc_content = get_html(url=url, mode="extract")
-            doc = parse_html(doc_content, "extract")
-            if doc:
-                retrieved_docs.append({"title": title, "doc": doc})
-            else:
-                continue
+            for dataset in datasets:
+                retrieved_docs.append(self.keyword_retrieval(dataset,top_weighted_keywords))
+                
         return retrieved_docs
+    
+    def keyword_retrieval(self, dataset:dict,top_weighted_keywords:list) -> List[List[str]]:
+        fields_to_search = self.fields_to_search
+        keyword_docs = []
+        for document in dataset:
+            matched_keywords_count = 0
+            for keyword in top_weighted_keywords:
+                if any(keyword in str(document.get(field, "")) for field in fields_to_search):
+                    matched_keywords_count += 1
+            # 如果关键词匹配度超过50%,则将文档添加到检索结果
+            if matched_keywords_count / len(top_weighted_keywords) >= 0.2:
+                doc_content = document.get("content", "") 
+                ans_content = document.get("answer", "")
+                if ans_content:
+                    doc_content = doc_content + document.get("answer", "")
+                keyword_docs.append((doc_content, matched_keywords_count))
+        keyword_docs.sort(key=lambda x: x[1], reverse=True)
+        return [doc[0] for doc in keyword_docs[:self.topk]]
+    
+    def bing_retrieval(self, query_list: List[str]) -> List[List[str]]:
+        retrieved_docs = []
+        for query in query_list:
+            doc = self.retrieve_with_engine(query,'bing')
+            retrieved_docs.append(doc)
+        return retrieved_docs
+    
+    def baidu_retrieval(self, query_list: List[str]) -> List[List[str]]:
+        retrieved_docs = []
+        for query in query_list:
+            doc = self.retrieve_with_engine(query,'baidu')
+            retrieved_docs.append(doc)
+        return retrieved_docs
+    
+    def retrieve_with_engine(self, query: str, mode='bing') -> List[str]:
+        if mode == 'bing':
+            search_results = bing_search(query)
+        elif mode == 'baidu':
+            search_results = baidu_search(query)
+        if search_results:
+            retrieved_docs = get_docs(search_results)
+        
+        return retrieved_docs[:self.topk]
+    
